00eab803c1b6f142577f85ab7bd00e50afacee62
chicotemaa <45741883+chicotemaa@users.noreply.github.com>
2020-10-15 14:37:12 -0300

    index
    index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "controllers/template.php";
+require_once "controllers/users.php";
+require_once "models/users.php";
+
+$template = new TemplateController();
+$template -> template();
